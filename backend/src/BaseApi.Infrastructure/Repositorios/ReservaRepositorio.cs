9fb5b7e5425b92a9d42b575856283fee42857a29
Lucas Estoco <lucasestoco22@gmail.com>
2026-06-17 21:49:51 -0300

    feat
    Atualiza projeto .NET com novas entidades e repositórios
    
    - Atualiza .gitignore para incluir arquivos e diretórios não versionados.
    - Modifica `appsettings.json` para alterar a string de conexão do MySQL.
    - Adiciona classes `Quadra` e `Reserva` com suas propriedades.
    - Define interfaces de repositório para `Quadra` e `Reserva`.
    - Implementa classes de configuração para mapeamento no Entity Framework Core.
    - Cria repositórios `QuadraRepositorio` e `ReservaRepositorio` com métodos não implementados.

---
+++ backend/src/BaseApi.Infrastructure/Repositorios/ReservaRepositorio.cs
@@ -0,0 +1,59 @@
+﻿using BaseApi.Domain.Entidades;
+using BaseApi.Domain.Interfaces.Repositorios;
+using BaseApi.Infrastructure.Dados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseApi.Infrastructure.Repositorios
+{
+    public class ReservaRepositorio(AppDbContext contexto) : IReservaRepositorio
+    {
+        public Task AdicionarAsync(Reserva reserva, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Reserva reserva)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HorarioDisponivelAsync(Guid quadraId, DateTime dataAgendada, TimeSpan horarioAgendado, Guid? ignorarId = null, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(IEnumerable<Reserva> Itens, int Total)> ListarAsync(int pagina, int tamanhoPagina, Guid? quadraId, DateTime? dataInicio, DateTime? dataFim, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Reserva>> ObterPorDataAsync(Guid quadraId, DateTime data, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Reserva?> ObterPorIdAsync(Guid id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Reserva>> ObterPorQuadraAsync(Guid quadraId, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(Reserva reserva)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SalvarAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
